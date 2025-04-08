61c9dbf7a17003a693e34c884aaa89794ed2a1f2
Tewahido <tewahido.tefera@kcpivot.co.za>
2025-04-08 16:44:36 +0200

    Made changes to App.jsx

--- Abc-Publishing-Frontend/src/App.jsx
+++ Abc-Publishing-Frontend/src/App.jsx
@@ -5,9 +5,14 @@ import { GetSection } from "./services/apiService";
 function App() {
   const [currentSection, setCurrentSection] = useState(null);
 
+  function updateUrl(sectionName) {
+    window.history.pushState({}, "", `/section/${sectionName}`);
+  }
+
   const fetchSection = async (sectionName = "preface") => {
     const data = await GetSection(sectionName);
     setCurrentSection(data);
+    updateUrl(sectionName);
   };
 
   useEffect(() => {
@@ -16,11 +21,11 @@ function App() {
 
   function handleClick(link) {
     fetchSection(link.section);
+    updateUrl(sectionName);
   }
 
   const links = currentSection?.navigation;
   const content = currentSection?.content;
-
   return (
     <>
       <h1>{currentSection?.title}</h1>
