426d9f017e94f4b9a0b016b78ddacfbeb802b250
Thobani Mjiyakho <thobani.mjiyakho@kcpivot.co.za>
2025-04-10 14:44:35 +0200

    added test for get section

--- ABC.Publishing.API.Test/SectionControllerTest.cs
+++ ABC.Publishing.API.Test/SectionControllerTest.cs
@@ -1,19 +1,45 @@
 ﻿using ABC.Publishing.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 
-namespace ABC.Publishing.API.Test;
-
-public class Tests
+namespace ABC.Publishing.API.Test
 {
-    private SectionController _sectionController;
-
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        private SectionController _sectionController;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void SetUp()
+        {
+            _sectionController = new SectionController();
+        }
+
+        [Test]
+        public void GIVEN_ExistingSectionName_WHEN_GetSectionIsCalled_THEN_ReturnsOkWithContent()
+        {   
+            //Arrange
+            var sectionName = "bohemia-chapter-1";
+            
+            //Act
+            var result = _sectionController.GetSection(sectionName);
+
+            //Asert
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result as OkObjectResult;
+            Assert.That(okResult?.Value, Is.Not.Null);
+        }
+
+        [Test]
+        public void GIVEN_NonExistentSectionName_WHEN_GetSectionIsCalled_THEN_ReturnsNotFound()
+        {
+            //Arrange
+            string sectionName = "thobani-section";
+
+            //Act
+            var result = _sectionController.GetSection(sectionName);
+
+            //Assert
+            Assert.That(result, Is.InstanceOf<NotFoundResult>());
+        }
     }
 }
